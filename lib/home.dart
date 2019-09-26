6cb01c4acb3164e38e33931ab544c076e715f14c
Madhukesh <madhukesh04@gmail.com>
2019-09-26 10:30:39 +0530

    added button widget

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,44 @@
+import 'package:firebase_login/signInButtonWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hello World!"),
+      backgroundColor: Colors.white,
+      body: Container(
+        decoration: BoxDecoration(
+            color: Colors.purpleAccent,
+            gradient: LinearGradient(colors: [Colors.blue, Colors.red])),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SvgPicture.asset(
+                'assets/icons/firebase.svg',
+                height: 180,
+                width: 180,
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              SignInButton(
+                title: 'Sign in with Google',
+                assetName: 'assets/icons/search.svg',
+                onTap: () {},
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              SignInButton(
+                title: "Sign in with Facebook",
+                assetName: 'assets/icons/facebook.svg',
+                onTap: () {},
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/signInButtonWidget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SignInButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String title;
+  final String assetName;
+
+  SignInButton({
+    @required this.onTap,
+    this.title = '',
+    this.assetName = '',
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      color: Colors.white,
+      splashColor: Colors.transparent,
+      onPressed: () {},
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      highlightElevation: 0,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SvgPicture.asset(
+              assetName,
+              height: 30,
+              width: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                title,
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
