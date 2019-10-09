8a8c25aa4024fbf1213bf0ffc356b69bc1b1e94d
Madhukesh <madhukesh04@gmail.com>
2019-10-09 20:05:14 +0530

    fixed orientation issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:firebase_login/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Loginpage()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Loginpage());
   }
 }
